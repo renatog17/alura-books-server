a19fef3bb2149d4d0bfe3abd3c615d9d7ab6e9a4
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-05-30 14:49:24 -0300

    status code 422

--- alura-books-server/controladores/livro.js
+++ alura-books-server/controladores/livro.js
@@ -13,8 +13,13 @@ function getLivros(req, res) {
 function getLivro(req, res) {
     try {
         const id = req.params.id
-        const livro = getLivroPorId(id)
-        res.send(livro)
+        if(id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -24,9 +29,14 @@ function getLivro(req, res) {
 function postLivro(req, res) {
     try {
         const livroNovo = req.body
-        insereLivro(livroNovo)
-        res.status(201)
-        res.send('livro inserido com sucesso')
+        if(req.body.nome) {
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("Livro inserido com sucesso")
+        } else {
+            res.status(422)
+            res.send("O campo nome é obrigatório")
+        }
     } catch(error) {
         res.status(500)
         res.send(error.message)
@@ -36,10 +46,14 @@ function postLivro(req, res) {
 function patchLivro(req, res) {
     try {
         const id = req.params.id
-        const body = req.body
-
-        modificaLivro(body, id)
-        res.send("Item modificado com sucesso")
+        if(id && Number(id)) {
+            const body = req.body
+            modificaLivro(body, id)
+            res.send("Item modificado com sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -47,11 +61,15 @@ function patchLivro(req, res) {
 }
 
 function deleteLivro(req, res) {
-    console.log('oi')
     try {
         const id = req.params.id
-        deletaLivroPorId(id)
-        res.send("livro deletado com sucesso")
+        if(id && Number(id)) {
+            deletaLivroPorId(id)
+            res.send("livro deletado com sucesso")
+        } else {
+            res.status(422)
+            res.send("ID inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
